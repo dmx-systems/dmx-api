35290d7cf34486eecc8986e1b3ad703c4e8085b3
Jörg Richter <jri@deepamehta.de>
2018-02-11 23:40:05 +0100

    REST client: getAssocWithPlayers()

--- src/permission-cache.js
+++ src/permission-cache.js
@@ -7,9 +7,9 @@ import restClient from './rest-client'
 //   }
 //
 // Note: at client-side there is no explicit READ permission.
-// The Webclient never gets hold of an object the user is not allowed to read.
+// The webclient never gets hold of an object the user is not allowed to read.
 // The server would not send it in the first place.
-// ### TODO: not fully true. Think about logout: the Webclient might still hold
+// ### TODO: not fully true. Think about logout: the webclient might still hold
 // objects which are not readable anymore.
 var permissionCache = {}
 
