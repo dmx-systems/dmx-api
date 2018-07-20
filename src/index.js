db6d1511a6e1ed3f508c9a723057dfea5ff9243b
Jörg Richter <jri@deepamehta.de>
2018-07-20 18:44:05 +0200

    Test browser cache

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import typeCache  from './type-cache'
 import permCache  from './permission-cache'
 import utils      from './utils'
 
-console.log('DM5 API 2018/07/20.1')
+console.log('DM5 API 2018/07/20.2')
 
 export default {
 
